d6150f32fff3d680f1fd0b5d82277e7e2f0db485
LSSanches <101747287+LSSanches@users.noreply.github.com>
2022-10-27 15:28:40 -0400

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was created by LSanches and its open-sourced at
+        <a
+          href="https://github.com/LSSanches/project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
- 
