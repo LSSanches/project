d30c8bf6b21da7493ed3cc346026e47f2dff07ef
laura_sanches <laurassanches@gmail.com>
2022-11-21 16:52:26 -0500

    css adjustments

--- src/Weather.js
+++ src/Weather.js
@@ -57,7 +57,7 @@ export default function Weather(props) {
               <input
                 type="submit"
                 value="Search"
-                className="btn btn-primary w-100"
+                className="btn btn-primary w-80"
               />
             </div>
           </div>
